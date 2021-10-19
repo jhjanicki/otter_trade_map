96ac87376a5f124f3d09f207e419eac7f85a1a07
Julia <>
2021-10-19 14:29:34 +0200

    first commit

---
+++ profilesData.js
@@ -0,0 +1,42 @@
+var profilesData =[
+{
+"id": 1,
+"species":"Aonyx cinereus",
+"name":"Asian Small-clawed Otter",
+"description":"",
+"range":"",
+"status":"Vulnerable",
+"img":"./img/img1.jpg",
+"trade":"Pelt, pet (“otter cafes”)"
+},
+{
+"id": 2,
+"species":"Lutra lutra",
+"name":"Eurasian otter",
+"description":"",
+"range":"",
+"status":"Near Threatened",
+"img":"./img/img2.jpg",
+"trade":"Pelt, meat and body parts"
+},
+{
+"id": 3,
+"species":"Lutra sumatrana",
+"name":"Hairy-nosed Otter",
+"description":"",
+"range":"",
+"status":"Endangered",
+"img":"./img/img3.jpg",
+"trade":"Pelt, meat and body parts (for medicinal use), pet (rare but detrimental for this species because of its status and range)"
+},
+{
+"id": 4,
+"species":"Lutrogale perspicillata",
+"name":"Smooth-coated Otter",
+"description":"",
+"range":"",
+"status":"Vulnerable",
+"img":"./img/img4.jpg",
+"trade":"Pelt, meat and body parts,pet (second most traded as pet of the 4 Asian otter species)"
+}
+]
